a3faf323ea1f1f4bb5a7b3b4f9a39e2ec299e025
baboonwu <wuhao@augmn.com>
2016-03-08 16:51:10 +0800

    add timeout

--- api_test.go
+++ api_test.go
@@ -3,6 +3,7 @@ package foursquare
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"testing"
 )
 
@@ -10,17 +11,28 @@ const CLIENTID = ""
 const CLIENTSECRET = ""
 
 func Test_SearchNear(t *testing.T) {
+
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+
 	api := NewApi(CLIENTID, CLIENTSECRET)
-	resp, _ := api.Search(&SearchReq{Near: "朝阳区"})
+	resp, e := api.Search(&SearchReq{Near: "朝阳区"})
+	if e != nil {
+		t.Error("Test_SearchNear")
+		return
+	}
+	fmt.Println(resp.Meta)
 	assert.NotNil(t, resp)
 	assert.NotEmpty(t, resp)
-	fmt.Println(resp.Meta)
 }
 
 func Test_SearchLatLng(t *testing.T) {
 	api := NewApi(CLIENTID, CLIENTSECRET)
-	resp, _ := api.Search(&SearchReq{LatLng: "31.33,109.312"})
+	resp, e := api.Search(&SearchReq{LatLng: "31.33,109.312"})
+	if e != nil {
+		t.Error("Test_SearchLatLng")
+		return
+	}
+	fmt.Println(resp.Meta)
 	assert.NotNil(t, resp)
 	assert.NotEmpty(t, resp)
-	fmt.Println(resp.Meta)
 }
